1d3e093b68e1c3e7838d65544c26d1e22180f401
Nitin Sharma <sharmanitinkumar782@gmail.com>
2026-07-06 12:50:37 +0530

    Added request logger

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+// Request Logger
+app.use((req, res, next) => {
+    console.log(`[REQUEST] ${req.method} ${req.url}`);
+    next();
+});
 // Paths
 const cppDir = path.join(__dirname, '..', 'cpp');
 const binDir = path.join(cppDir, 'bin');
